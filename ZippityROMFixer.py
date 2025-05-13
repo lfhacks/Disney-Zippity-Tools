bf65c5484a3378234a7ec75bf087ad7bd072eb41
Dr. RNG <33909717+BLiNXthetimesweeperGOD@users.noreply.github.com>
2025-05-13 15:05:04 -0400

    Add more descriptive comments

--- ZippityROMFixer.py
+++ ZippityROMFixer.py
@@ -1,18 +1,29 @@
 #LeapFrog Disney Zippity ROM fixer
 from codingTools import *
 
+#Open the ROM file
 ROM = dialogs.file()
+
+#Create the output directory
 outDir = fileTools.outputDirectory(ROM)
+
+#Get the size of the ROM file
 size = fileTools.size(ROM)
 
+#Open the ROM
 with open(ROM, "rb") as rom:
-    data = bytearray()
+    #The ROM contents minus the padding will get stored in this bytearray
+    data = bytearray() 
 
     try:
-        while rom.tell() < size:
+        while rom.tell() < size: #This gets looped until the end of the file
+            #Extend the data bytearray with 0x200 bytes
             data.extend(rom.read(0x200))
+            
+            #Skip the 16 bytes of padding that follows
             rom.seek(16, 1)
     except EOFError:
+        #In case there ever somehow happens to be an end of file error
         print("EOF")
 
     #Write the fixed ROM
