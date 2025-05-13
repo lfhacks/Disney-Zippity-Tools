bb7daa747334d73f91def50309a88df8948ad527
Dr. RNG <33909717+BLiNXthetimesweeperGOD@users.noreply.github.com>
2025-05-13 14:59:45 -0400

    Add more descriptive (and consistently formatted) comments

--- ZippitySoundPackageUnpacker.py
+++ ZippitySoundPackageUnpacker.py
@@ -1,7 +1,13 @@
 #(short name)_wavs.bin unpacker for LeapFrog Disney Zippity games
 from codingTools import *
-wavsBinary = dialogs.file() #Prompt user to open a file
-outDir = fileTools.outputDirectory(wavsBinary) #Generate the output directory
+
+#Prompt user to open a file
+wavsBinary = dialogs.file()
+
+#Generate the output directory
+outDir = fileTools.outputDirectory(wavsBinary)
+
+#Create the TXTH file (used so VGMStream can play these)
 txth = """codec = IMA                 
 sample_rate = @0x10$4
 channels = 1
@@ -9,12 +15,15 @@ start_offset = 0x28
 num_samples = @0x14"""
 fileTools.writeNewTextFile(outDir+".ALP.txth", txth)
 
+#Open the WAVS binary for extraction
 with open(wavsBinary, "rb") as wavs:
+    #Get the file count
     fileCount = LE_unpack.uint(wavs.read(4))
     for fileIndex in range(fileCount):
+        #Get the start offset and size of the file
         fileOffset, fileSize = LE_multiunpack.uint(wavs.read(8))
         
-        #Save offset so we can return later
+        #Save the current offset we're at in the table so we can return later
         last = wavs.tell() 
         
         #Go to and get the data
@@ -24,5 +33,5 @@ with open(wavsBinary, "rb") as wavs:
         #Save the data
         fileTools.writeNewBinaryFile(outDir+f'{fileIndex:04}.alp', fileData)
 
-        #Return to saved offset
+        #Return to the saved table offset before next loop
         wavs.seek(last) 
